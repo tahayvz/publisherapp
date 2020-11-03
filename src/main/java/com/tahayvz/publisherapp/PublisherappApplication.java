e3f7bfbdd76439c9a4161418fea9758f2cea0852
abc <abc@example.com>
2020-11-03 09:15:51 +0300

    Initial commit

---
+++ src/main/java/com/tahayvz/publisherapp/PublisherappApplication.java
@@ -0,0 +1,13 @@
+package com.tahayvz.publisherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublisherappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tahayvz/publisherapp/PublisherappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tahayvz.publisherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
